2644f57e6524fa297516d1c23e99054a96586b16
Matilda Brantley <matildahbrantley@gmail.com>
2021-10-26 20:16:48 -0400

    Updated About Me section.

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,17 @@ function About() {
     return (
       <div className="about lead">
           <h4 className="h4">About Me</h4>
-            <p>Hi, I'm Matilda Brantley, a web developer in training. 
-            I love seeing an idea in my mind come to life. 
-            Making software is the perfect outlet for this as it's one of the most mutable and living designs one could create. 
-            I also have a passion for the biological sciences, especially adaptation, evolution and intelligent systems. 
-            These inspire my ideals for software that can adjust to ever-changing needs.
-        </p>
-        <p>
-            I am now attending Georgia Tech's Full Stack Coding Bootcamp during my last week. My current projects
-             involve React, MongoDB, Progressive Web Apps, Phaser and AI.
-        </p>
+            <p>
+              Welcome! I'm Matilda Brantley, a full stack web developer. I love seeing an idea in my mind spring to life. 
+              Making software is the perfect outlet for this as it's one of the most living, mutable designs one could create. 
+              I also have a passion for the biological sciences, especially intelligent systems and evolving systems. These inspire my 
+              ideals for software that can adjust to ever-changing needs or generate novel and satisfying experiences for the user.
+            </p>
+            <p>
+              I recently completed Georgia Tech's Full Stack Coding Bootcamp and graduated with a Bachelor of Science
+              from the Evergreen State College and Rogers State University. My current projects
+              involve React, MongoDB, Progressive Web Apps, Phaser's game engine and Neural Networks.
+            </p>
       </div>
     );
   }
