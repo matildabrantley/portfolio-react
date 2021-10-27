f56e7fcd40312fc906266ac74532fd6a661a2b26
Matilda Brantley <matildahbrantley@gmail.com>
2021-10-26 21:24:11 -0400

    Final draft of About Me.

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,25 @@ function About() {
           <h4 className="h4">About Me</h4>
             <p>
               Welcome! I'm Matilda Brantley, a full stack web developer. I love seeing an idea in my mind spring to life. 
-              Making software is the perfect outlet for this as it's one of the most living, mutable designs one could create. 
-              I also have a passion for the biological sciences, especially intelligent systems and evolving systems. These inspire my 
-              ideals for software that can adjust to ever-changing needs or generate novel and satisfying experiences for the user.
+              Making software is the perfect outlet for this as it's one of the most living, mutable designs we make. 
+              I also have a passion for the biological sciences, especially intelligent systems and evolution. These inspire my 
+              ideals for software that can adjust to ever-changing needs and generate satisfying novel experiences for the user.
             </p>
             <p>
               I recently completed Georgia Tech's Full Stack Coding Bootcamp and graduated with a Bachelor of Science
-              from the Evergreen State College and Rogers State University. My current projects
-              involve React, MongoDB, Progressive Web Apps, Phaser's game engine and Neural Networks.
+              from Rogers State University and Evergreen State College. Frameworks and technologies I'm currently involved with
+              include React, MongoDB, Progressive Web Apps, Express/Node, GraphQL, Phaser and AI. My GaTech final project was a 
+              solo endeavor bringing together a full MERN stack, game engine and my custom neural network framework that trains agents 
+              to quickly solve a myriad of challenges and play most types of games.
+            </p>
+            <p>
+              My coding experience goes back over a decade as an enthusiastic hobbyist, coming from a family of coders and engineers. 
+              I started with C++ and Java in the late 2000s, writing simple neural nets. In university, I received 
+              a well-rounded CS education of both theory and application, discovering new paradigms and learning Haskell, C, C#, Python and JavaScript. 
+              I enjoyed working with JS so much I decided to specialize in full stack web development: designing apps for the web uniquely bridges
+              that divide between user and developer in ways both succinct and satisfying.
+            </p>
+            <p>
             </p>
       </div>
     );
