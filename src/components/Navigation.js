b947b490569c565b30b6d25bd928835a0213a704
Matilda Brantley <38068564+matildabrantley@users.noreply.github.com>
2021-11-07 23:54:31 -0500

    Cleaned up Navigation's links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,12 +10,9 @@ function Navigation() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="https://github.com/matildabrantley">GitHub</Nav.Link>
                     <NavDropdown title="Other Links" id="basic-nav-dropdown">
                     <NavDropdown.Item href="https://matildabrantley.github.io/portfolio/">Original Portfolio</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Link 2</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
                 </Navbar.Collapse>
